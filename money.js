1e58a57db68e1bcba7bfe58e1e6b6572e76d63bc
Scott Davies <scottdavies1983@gmail.com>
2020-12-16 06:38:56 +0000

    basic functionality complete

--- money.js
+++ money.js
@@ -1,7 +1,15 @@
 // eslint-disable-next-line quotes
 const fromSelect = document.querySelector('[name="from_currency"]');
 // eslint-disable-next-line quotes
+const fromInput = document.querySelector('[name="from_amount"]');
+// eslint-disable-next-line quotes
 const toSelect = document.querySelector('[name="to_currency"]');
+// eslint-disable-next-line quotes
+const toEl = document.querySelector(".to_amount");
+const form = document.querySelector(".app form");
+const endpoint = "https://api.exchangeratesapi.io/latest";
+
+const ratesByBase = {};
 
 const currencies = {
   USD: "United States Dollar",
@@ -47,7 +55,50 @@ function generateOptions(options) {
     .join("");
 }
 
+async function fetchRates(base = "USD") {
+  const res = await fetch(`${endpoint}?base=${base}`);
+  const rates = await res.json();
+  return rates;
+}
+
+async function convert(amount, from, to) {
+  // first check if we even have the rates to covert from that currency
+  if (!ratesByBase[from]) {
+    console.log(
+      `Oh no, we dont have ${from} to convert ${to}. So lets go get it`
+    );
+    const rates = await fetchRates(from);
+    console.log(rates);
+    // store them for nect time
+    ratesByBase[from] = rates;
+  }
+  // convert the amount that they passed in
+  const rate = ratesByBase[from].rates[to];
+  const convertedAmount = rate * amount;
+  console.log(`${amount} ${from} is ${convertedAmount} in ${to}`);
+  return convertedAmount;
+}
+
+function formatCurrency(amount, currency) {
+  return Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency,
+  }).format(amount);
+}
+
+async function handleInput(e) {
+  const rawAmount = await convert(
+    fromInput.value,
+    fromSelect.value,
+    toSelect.value
+  );
+
+  toEl.textContent = formatCurrency(rawAmount, toSelect.value);
+}
+
 const optionsHTML = generateOptions(currencies);
 // populate the options elements
 fromSelect.innerHTML = optionsHTML;
 toSelect.innerHTML = optionsHTML;
+
+form.addEventListener("input", handleInput);
