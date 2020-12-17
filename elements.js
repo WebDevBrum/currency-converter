774ed5bf8d9429b12219003ee6e05c92d08923e8
Scott Davies <scottdavies1983@gmail.com>
2020-12-17 18:56:04 +0000

    module refactoring in progress

---
+++ elements.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line quotes
+export const fromSelect = document.querySelector('[name="from_currency"]');
+// eslint-disable-next-line quotes
+export const fromInput = document.querySelector('[name="from_amount"]');
+// eslint-disable-next-line quotes
+export const toSelect = document.querySelector('[name="to_currency"]');
+// eslint-disable-next-line quotes
+export const toEl = document.querySelector(".to_amount");
--- money.js
+++ money.js
@@ -1,3 +1,7 @@
+import { generateOptions } from "./utils.js";
+import currencies from "./currencies.js";
+import { handleInput } from "./handlers.js";
+
 // eslint-disable-next-line quotes
 const fromSelect = document.querySelector('[name="from_currency"]');
 // eslint-disable-next-line quotes
@@ -10,91 +14,6 @@ const form = document.querySelector(".app form");
 const endpoint = "https://api.exchangeratesapi.io/latest";
 
 const ratesByBase = {};
-
-const currencies = {
-  USD: "United States Dollar",
-  AUD: "Australian Dollar",
-  BGN: "Bulgarian Lev",
-  BRL: "Brazilian Real",
-  CAD: "Canadian Dollar",
-  CHF: "Swiss Franc",
-  CNY: "Chinese Yuan",
-  CZK: "Czech Republic Koruna",
-  DKK: "Danish Krone",
-  GBP: "British Pound Sterling",
-  HKD: "Hong Kong Dollar",
-  HRK: "Croatian Kuna",
-  HUF: "Hungarian Forint",
-  IDR: "Indonesian Rupiah",
-  ILS: "Israeli New Sheqel",
-  INR: "Indian Rupee",
-  JPY: "Japanese Yen",
-  KRW: "South Korean Won",
-  MXN: "Mexican Peso",
-  MYR: "Malaysian Ringgit",
-  NOK: "Norwegian Krone",
-  NZD: "New Zealand Dollar",
-  PHP: "Philippine Peso",
-  PLN: "Polish Zloty",
-  RON: "Romanian Leu",
-  RUB: "Russian Ruble",
-  SEK: "Swedish Krona",
-  SGD: "Singapore Dollar",
-  THB: "Thai Baht",
-  TRY: "Turkish Lira",
-  ZAR: "South African Rand",
-  EUR: "Euro",
-};
-
-function generateOptions(options) {
-  return Object.entries(options)
-    .map(
-      ([currencyCode, currencyName]) =>
-        `<option value="${currencyCode}">${currencyCode} - ${currencyName}</option>`
-    )
-    .join("");
-}
-
-async function fetchRates(base = "USD") {
-  const res = await fetch(`${endpoint}?base=${base}`);
-  const rates = await res.json();
-  return rates;
-}
-
-async function convert(amount, from, to) {
-  // first check if we even have the rates to covert from that currency
-  if (!ratesByBase[from]) {
-    console.log(
-      `Oh no, we dont have ${from} to convert ${to}. So lets go get it`
-    );
-    const rates = await fetchRates(from);
-    console.log(rates);
-    // store them for nect time
-    ratesByBase[from] = rates;
-  }
-  // convert the amount that they passed in
-  const rate = ratesByBase[from].rates[to];
-  const convertedAmount = rate * amount;
-  console.log(`${amount} ${from} is ${convertedAmount} in ${to}`);
-  return convertedAmount;
-}
-
-function formatCurrency(amount, currency) {
-  return Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency,
-  }).format(amount);
-}
-
-async function handleInput(e) {
-  const rawAmount = await convert(
-    fromInput.value,
-    fromSelect.value,
-    toSelect.value
-  );
-
-  toEl.textContent = formatCurrency(rawAmount, toSelect.value);
-}
 
 const optionsHTML = generateOptions(currencies);
 // populate the options elements
