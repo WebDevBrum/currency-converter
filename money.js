10133f7abd6e4ea35fcb0b3964fdbfd9b87b869d
Scott Davies <scottdavies1983@gmail.com>
2020-12-18 18:42:43 +0000

    app updated to modular version

--- money.js
+++ money.js
@@ -1,23 +1,13 @@
 import { generateOptions } from "./utils.js";
 import currencies from "./currencies.js";
 import { handleInput } from "./handlers.js";
+import { fromSelect, toSelect } from "./elements.js";
+import { init } from "./init.js";
 
-// eslint-disable-next-line quotes
-const fromSelect = document.querySelector('[name="from_currency"]');
-// eslint-disable-next-line quotes
-const fromInput = document.querySelector('[name="from_amount"]');
-// eslint-disable-next-line quotes
-const toSelect = document.querySelector('[name="to_currency"]');
-// eslint-disable-next-line quotes
-const toEl = document.querySelector(".to_amount");
-const form = document.querySelector(".app form");
-const endpoint = "https://api.exchangeratesapi.io/latest";
+// start the app
+// init();
 
-const ratesByBase = {};
+// starts the app when moused over
+const app = document.querySelector(".app");
 
-const optionsHTML = generateOptions(currencies);
-// populate the options elements
-fromSelect.innerHTML = optionsHTML;
-toSelect.innerHTML = optionsHTML;
-
-form.addEventListener("input", handleInput);
+app.addEventListener("mouseenter", init, { once: true });
